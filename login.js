f3677a92243779b915b19adfb4717f1259daa673
Thaissa Silva <88891278+ThaissaSilva@users.noreply.github.com>
2022-05-25 18:26:37 +0100

    login js

---
+++ login.js
@@ -0,0 +1,8 @@
+document.querySelector("#show-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".close-btn").addEventListener("click", function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+
