a9a15cf3f04abcab8a5ccb6792ed3cf8e30ad106
Thaissa Silva <88891278+ThaissaSilva@users.noreply.github.com>
2022-05-25 20:00:00 +0100

    alteracao login

--- login.js
+++ login.js
@@ -1,8 +1,10 @@
-document.querySelector("#show-login").addEventListener("click", function(){
+document.querySelector("#login").addEventListener("click", function(){
     document.querySelector(".popup").classList.add("active");
+    document.querySelector(".wrapper").classList.add("active");
 });
 
 document.querySelector(".close-btn").addEventListener("click", function(){
     document.querySelector(".popup").classList.remove("active");
+    document.querySelector(".wrapper").classList.remove("active");
 });
 
