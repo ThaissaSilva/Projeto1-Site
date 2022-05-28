cddd5288a2fd3b1e2b092941631e5281ec2e23a4
Thaissa Silva <88891278+ThaissaSilva@users.noreply.github.com>
2022-05-28 22:55:27 +0100

    alteração equipa

--- login.js
+++ login.js
@@ -1,15 +1,11 @@
-document.querySelector("#login .fa-magnifying-glass").addEventListener("click", function(){
-    document.querySelector(".popup").classList.add("active");
+document.querySelector("#login").addEventListener("click", function(){
     document.querySelector(".wrapper").classList.add("active");
+    document.querySelector(".popup").classList.add("active");
 });
 
 document.querySelector(".close-btn").addEventListener("click", function(){
     document.querySelector(".popup").classList.remove("active");
     document.querySelector(".wrapper").classList.remove("active");
 });
-
-
-//imagens EQUIPA
-
 
 
---
+++ projeto.js
@@ -0,0 +1,3 @@
+document.querySelector("#header-btInfo").addEventListener("click" ,function() {
+    document.querySelector(".header-cookie-shadow").classList.add('header-cookie-hide')
+})
