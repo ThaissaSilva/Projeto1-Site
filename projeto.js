c4fd5c8f0f4c3c64519176fc3c238252a4eb22b6
Thaissa Silva <88891278+ThaissaSilva@users.noreply.github.com>
2022-05-29 18:53:47 +0100

    login/logout

--- projeto.js
+++ projeto.js
@@ -1,6 +1,6 @@
 //Cookie
-document.querySelector("#header-btInfo").addEventListener("click" ,function() {
-    document.querySelector(".header-cookie-shadow").classList.add('header-cookie-hide')
+document.querySelector("#btInfo").addEventListener("click" ,function() {
+    document.querySelector(".cookie-shadow").classList.add('cookie-hide')
 })
 
 //Login
@@ -13,6 +13,27 @@ document.querySelector(".close-btn").addEventListener("click", function(){
     document.querySelector(".popup").classList.remove("active");
     document.querySelector(".wrapper").classList.remove("active");
 });
+
+
+//LOGIN
+function gerenciarSessao(data){
+    let texto = document.querySelector("#texto-bemvindo");
+
+    sessionStorage.setItem("id", data.id)
+    sessionStorage.setItem("nome", data.nome)
+    sessionStorage.setItem("email", data.email)
+
+    texto.innerHTML = `Bem vindo(a), ${data.nome}!`;
+
+    document.querySelector("#login").classList.remove("active");
+    document.querySelector("#logout").classList.add("active");
+}
+
+function logarEnter(event){
+    if (event.key === "Enter") {
+        logar();
+    }
+}
 
 //validar o login
 function logar(){
@@ -34,7 +55,43 @@ function logar(){
     fetch(`http://localhost:3000/utilizadores?email=${email}&=${senha}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            if(data.length <= 0){
+                sectionErros.innerHTML = "Utilizador inexistente!";
+                return;  
+            }
+            document.querySelector(".popup").classList.remove("active");
+            document.querySelector(".wrapper").classList.remove("active");
+
+            gerenciarSessao(data[0]);   
         })
 }
 
+function inicio(){
+    const id = sessionStorage.getItem("id");
+    const nome = sessionStorage.getItem("nome");
+    const email = sessionStorage.getItem("email");
+
+    console.log(id, nome, email);
+
+    if(id && nome && email){
+        let texto = document.querySelector("#texto-bemvindo");
+
+        texto.innerHTML = `Bem vindo(a), ${nome}!`;
+
+        document.querySelector("#login").classList.remove("active");
+        document.querySelector("#logout").classList.add("active");
+    }
+};
+
+function logout(){
+    sessionStorage.clear();
+
+    let texto = document.querySelector("#texto-bemvindo");
+
+    texto.innerHTML = "";
+
+    document.querySelector("#login").classList.add("active");
+    document.querySelector("#logout").classList.remove("active");
+}
+
+inicio();
